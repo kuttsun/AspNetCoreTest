cd7ce740f7f3e6bfb56579493df5771dbb423fb6
kuttsun <tsukushimail+github@gmail.com>
2018-03-26 17:16:28 +0900

    RuntimeIdentifiers を追加

---
+++ RuntimeIdentifiers/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RuntimeIdentifiers.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
