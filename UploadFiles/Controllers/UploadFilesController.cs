f96854c5f36fa9e793b15ec41e55bfd1fd081158
kuttsun <tsukushimail+github@gmail.com>
2018-06-29 11:28:18 +0900

    UploadFiles を追加

---
+++ UploadFiles/Controllers/UploadFilesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace UploadFiles.Controllers
+{
+    public class UploadFilesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("UploadFiles")]
+        public async Task<IActionResult> Post(List<IFormFile> files)
+        {
+            // ファイルサイズの取得
+            long size = files.Sum(f => f.Length);
+
+            // full path to file in temp location
+            // 一時ファイルへのパスを取得
+            var filePath = Path.GetTempFileName();
+
+            foreach (var file in files)
+            {
+                // ファイルが空でない場合
+                if (file.Length > 0)
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        // ファイルの内容をストリームにコピー
+                        await file.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            // process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            return Ok(new { count = files.Count, size, filePath });
+        }
+    }
+}
