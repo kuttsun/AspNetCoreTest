a52c9196854882b3561b31bd85da4fc1fecc6dbe
kuttsun <tsukushimail+github@gmail.com>
2018-07-05 11:44:47 +0900

    DI によるスコープのテスト

---
+++ DependencyInjection/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DependencyInjection.Models;
+
+namespace DependencyInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly OperationService _operationService;
+        private readonly IOperationTransient _transientOperation;
+        private readonly IOperationScoped _scopedOperation;
+        private readonly IOperationSingleton _singletonOperation;
+        private readonly IOperationSingletonInstance _singletonInstanceOperation;
+
+        public HomeController(OperationService operationService,
+            IOperationTransient transientOperation,
+            IOperationScoped scopedOperation,
+            IOperationSingleton singletonOperation,
+            IOperationSingletonInstance singletonInstanceOperation)
+        {
+            _operationService = operationService;
+            _transientOperation = transientOperation;
+            _scopedOperation = scopedOperation;
+            _singletonOperation = singletonOperation;
+            _singletonInstanceOperation = singletonInstanceOperation;
+        }
+
+        public IActionResult Index()
+        {
+            // viewbag contains controller-requested services
+            ViewBag.Transient = _transientOperation;
+            ViewBag.Scoped = _scopedOperation;
+            ViewBag.Singleton = _singletonOperation;
+            ViewBag.SingletonInstance = _singletonInstanceOperation;
+
+            // operation service has its own requested services
+            ViewBag.Service = _operationService;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DependencyInjection/Operation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+
+    public class Operation : IOperation, IOperationTransient, IOperationScoped, IOperationSingleton, IOperationSingletonInstance
+    {
+        public Guid OperationId { get; set; }
+
+        // コンストラクタで GUID を受け取る、提供されない場合は新規に GUID を作成
+        public Operation() : this(Guid.NewGuid())
+        {
+        }
+        public Operation(Guid id)
+        {
+            OperationId = id;
+            Console.WriteLine(OperationId);
+        }
+    }
+}
