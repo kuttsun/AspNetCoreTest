469f7ecb506d8f7790344f82e4d038e7621b4b68
kuttsun <tsukushimail+github@gmail.com>
2019-04-24 09:14:17 +0900

    Basic 追加

---
+++ Basic/Models/SampleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basic.Models
+{
+    public class SampleViewModel
+    {
+        public IReadOnlyList<Person> Persons { get; set; }
+    }
+}
